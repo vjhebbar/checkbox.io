48587766780efbc3d2f28ced9284cf11934d593c
Rahul Coutinho <rcoutin@ncsu.edu>
2018-04-10 02:09:01 +0000

    Testing

--- public_html/js/ie-fix.js
+++
@@ -1,9 +0,0 @@
-if(!(window.console && console.log)) {
-  console = {
-    log: function(){},
-    debug: function(){},
-    info: function(){},
-    warn: function(){},
-    error: function(){}
-  };
-}
